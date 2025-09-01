d86d7e4a5cdeec9319672780a9ede070144a188b
jsampaiog <jose@cobogo.tech>
2025-09-01 23:32:33 +0200

    commit

---
+++ installation-pod-map-dp/semantic-model/model/sqls/installation_pod_map.sql
@@ -0,0 +1,6 @@
+-- Source Aligned Data Product: dim_installation_last
+-- This represents the diminstallationlast-dp data product
+
+SELECT
+    installation_last_ext_pod_id
+FROM "icebase"."bottomup"."installation_pod_map" 
